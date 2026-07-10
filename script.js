b0d7ad38b930318c5549d3317f786451ceee79ef
insufficientcreator <blackr1c3.w@gmail.com>
2026-07-10 10:23:08 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ const outDot2 = document.getElementById('out-dot2');
 const outDot3 = document.getElementById('out-dot3');
 const btnSave = document.getElementById('btn-save');
 
+
 inputDot1.addEventListener('input', (e) => outDot1.style.backgroundColor = e.target.value);
 inputDot2.addEventListener('input', (e) => outDot2.style.backgroundColor = e.target.value);
+
 inputDot3.addEventListener('input', (e) => {
     const nextColor = e.target.value;
     outDot3.style.backgroundColor = nextColor;
     btnSave.style.backgroundColor = nextColor;
-    
-    const chNameBorders = document.querySelectorAll('.info-name');
-    chNameBorders.forEach(el => el.style.borderColor = nextColor);
 });
 
 const inputBoxBg = document.getElementById('input-box-bg');
